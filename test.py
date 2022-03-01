538cd137cb1bf02a311446e28db41c18d62c3a3a
Thomas Woodside <thomas.woodside@gmail.com>
2022-03-01 15:52:33 -0500

    add ConTRoL

--- test.py
+++ test.py
@@ -1,5 +1,3 @@
 from sentence_transformers import CrossEncoder
 model = CrossEncoder("facebook/bart-large-mnli")
-print(model.predict(["He likes red.", "She likes red."]))
-print(model.predict(["He likes red.", "He likes red."]))
-print(model.predict(["He likes red.", "He doesn't like red."]))
+
