cc6d673c33e2d34dae71f2352ddb82453ff0a65d
Thomas Woodside <thomas.woodside@gmail.com>
2022-03-01 15:57:26 -0500

    use correct names

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ total = 0
 with open("data/test.jsonl", "r") as f:
     for line in f:
         data = json.loads(line)
-        prediction = model.predict([data["sentence1"], data["sentence2"]])
+        prediction = model.predict([data["premise"], data["hypothesis"]])
         argmax = prediction.argmax()
         correct += data["label"] == "e" and argmax == 2 or data["label"] == "c" and argmax == 0 or data["label"] == "n" and argmax == 1
         total += 1
