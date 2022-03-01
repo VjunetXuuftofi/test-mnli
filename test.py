c53793c3f3874a2cbc324115b4abc5415fc69cdb
Thomas Woodside <thomas.woodside@gmail.com>
2022-03-01 15:56:21 -0500

    simple test

--- test.py
+++ test.py
@@ -1,3 +1,13 @@
 from sentence_transformers import CrossEncoder
+import json
 model = CrossEncoder("facebook/bart-large-mnli")
-
+correct = 0
+total = 0
+with open("data/test.jsonl", "r") as f:
+    for line in f:
+        data = json.loads(line)
+        prediction = model.predict([data["sentence1"], data["sentence2"]])
+        argmax = prediction.argmax()
+        correct += data["label"] == "e" and argmax == 2 or data["label"] == "c" and argmax == 0 or data["label"] == "n" and argmax == 1
+        total += 1
+        print("Accuracy:", correct / total)
