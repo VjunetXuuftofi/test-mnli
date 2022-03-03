88ec25568ecd9df6ce7cd457e754de1c4ef5bcce
Thomas Woodside <thomas.woodside@gmail.com>
2022-03-03 16:32:36 -0500

    try validation

--- finetune.py
+++ finetune.py
@@ -33,7 +33,7 @@ trainer = Trainer(
     model=nli_model,
     args=training_args,
     train_dataset=tokenized_data["train"],
-    eval_dataset=tokenized_data["val"],
+    eval_dataset=tokenized_data["validation"],
     tokenizer=tokenizer,
     data_collator=data_collator,
 )
