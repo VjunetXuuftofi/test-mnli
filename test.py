73e2b2e397d05423c8a09d2fda46d0e700192f96
Thomas Woodside <thomas.woodside@gmail.com>
2022-03-07 10:45:38 -0500

    run testing

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ def tokenize_function(examples):
 
 # bart-large: 0.4869
 # deberta-v2-xxlarge-mnli: 0.4608
-model = CrossEncoder("microsoft/deberta-v2-xxlarge-mnli")
+model = CrossEncoder("/data/uid1803058/models/checkpoint-42000", tokenize_function=tokenize_function)
 correct = 0
 total = 0
 with open("data/test.jsonl", "r") as f:
